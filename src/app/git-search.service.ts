b08da548464d3c942257f3fa389ace3876931aff
Eva Stolz <evasmail@gmail.com>
2019-07-20 23:28:46 +0200

    Add service

---
+++ src/app/git-search.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { GitSearch } from './git-search';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GitSearchService {
+  cachedValues: Array<{
+          [query: string]: GitSearch
+      }> = [];
+
+  constructor() { }
+
+  gitSearch = (query: string) => {
+    let promise = new Promise((resolve, reject) => {
+      if (this.cachedValues[query]) {
+        resolve(this.cachedValues[query])
+        }
+      else {
+        resolve("Placeholder");
+      }    
+    })
+    return promise;
+  }
+}
